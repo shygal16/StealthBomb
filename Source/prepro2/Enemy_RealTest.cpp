31b3766e6a375fa8f2cd7520135ba8c7a03b2f97
andchelsea <andchelsea@gmail.com>
2016-05-19 19:47:51 -0700

    FINALLY fixed the enemy issue for good?

--- Source/prepro2/Enemy_RealTest.cpp
+++ Source/prepro2/Enemy_RealTest.cpp
@@ -14,28 +14,29 @@ AEnemy_RealTest::AEnemy_RealTest()
 	, sightConfig(CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("AI Sight")))
 	, soundConfig(CreateDefaultSubobject<UAISenseConfig_Hearing>(TEXT("AI Hearing")))
 {
- 
-	
+
+	perceptionComponent->ConfigureSense(*sightConfig);
+	perceptionComponent->ConfigureSense(*soundConfig);
+	perceptionComponent->SetDominantSense(sightConfig->GetSenseImplementation());
+
 	perceptionComponent->OnPerceptionUpdated.AddDynamic(this, &AEnemy_RealTest::SenseStuff);
-	
-	sightConfig->SightRadius = 4000.0f;
-	sightConfig->LoseSightRadius = 4020.f;
-	sightConfig->PeripheralVisionAngleDegrees = 360.0f;
+
+	sightConfig->SightRadius = 3000.0f;
+	sightConfig->LoseSightRadius = 3500.f;
+	sightConfig->PeripheralVisionAngleDegrees = 90.0f;
 	sightConfig->DetectionByAffiliation.bDetectEnemies = true;
 	sightConfig->DetectionByAffiliation.bDetectNeutrals = true;
 	sightConfig->DetectionByAffiliation.bDetectFriendlies = true;
 
-	soundConfig->HearingRange = 4000.0f;
+	perceptionComponent->ConfigureSense(*sightConfig);
+
+	soundConfig->HearingRange = 400.0f;
 	soundConfig->bUseLoSHearing = false;
 	soundConfig->DetectionByAffiliation.bDetectEnemies = true;
 	soundConfig->DetectionByAffiliation.bDetectNeutrals = true;
 	soundConfig->DetectionByAffiliation.bDetectFriendlies = true;
-	
+
 	perceptionComponent->ConfigureSense(*soundConfig);
-	perceptionComponent->ConfigureSense(*sightConfig);
-
-	perceptionComponent->SetDominantSense(sightConfig->GetSenseImplementation());
-
 
 	PrimaryActorTick.bCanEverTick = true;
 
@@ -45,16 +46,19 @@ AEnemy_RealTest::AEnemy_RealTest()
 void AEnemy_RealTest::BeginPlay()
 {
 	Super::BeginPlay();
-	UAIPerceptionSystem::RegisterPerceptionStimuliSource(this, sightConfig->GetSenseImplementation(), Controller->GetPawn());
-	UAIPerceptionSystem::RegisterPerceptionStimuliSource(this, soundConfig->GetSenseImplementation(), Controller->GetPawn());
-	
+
+	// UGameplayStatics::GetPlayerCharacter(GetWorld(),0) vs Controller->GetControlledPawn()
+	//UAIPerceptionSystem::RegisterPerceptionStimuliSource(this, sightConfig->GetSenseImplementation(), this);
+	//UAIPerceptionSystem::RegisterPerceptionStimuliSource(this, sightConfig->GetSenseImplementation(), UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	//UAIPerceptionSystem::RegisterPerceptionStimuliSource(this, soundConfig->GetSenseImplementation(),this);
+
 }
-
 // Called every frame
-void AEnemy_RealTest::Tick( float DeltaTime )
+void AEnemy_RealTest::Tick(float DeltaTime)
 {
-	Super::Tick( DeltaTime );
-
+	Super::Tick(DeltaTime);
+	FRotator rot = FRotator(0, 1, 0);
+	SetActorRotation(GetActorRotation() + rot);
 }
 
 // Called to bind functionality to input
@@ -68,4 +72,9 @@ void AEnemy_RealTest::SenseStuff(TArray<AActor*> testActors)
 {
 	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, "I see you!");
 	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Blue, "I hear you!");
+
+	FVector Movement = GetActorLocation() - testActors[0]->GetActorLocation();
+
+	Movement /= 3;
+	SetActorLocation(GetActorLocation() - Movement);
 }
